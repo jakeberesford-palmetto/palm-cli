f858b802993e24423ebe1d903d45b3d119d49408
Jake Beresford <jake.beresford@palmetto.com>
2023-03-13 17:12:08 -0400

    Make palm update less dumb
    - Simplify command string
    - use env.run_on_host
    - proper exit codes

--- palm/plugins/core/commands/cmd_update.py
+++ palm/plugins/core/commands/cmd_update.py
@@ -1,30 +1,20 @@
-import shlex
-import subprocess
+import sys
 
 import click
 
 
 @click.command("update")
 @click.option("--local", is_flag=True, help="attempts to install from cwd")
-def cli(local: bool):
+@click.pass_obj
+def cli(environment, local: bool):
     """This updates the current version of palm. meta huh?"""
 
-    if not click.confirm(
-        f"\nAttempt to update Palm {'from cwd' if local else 'from github'}?"
-    ):
-        click.secho("Cancelled", fg="red")
-        return
+    cmd = "python3 -m pip install ." if local else "pip install palm -U"
 
-    uninstall_cmd = "python3 -m pip uninstall -y palm"
-    if local:
-        upgrade_cmd = f"python3 -m pip install ."
+    ex_code, _, _ = environment.run_on_host(cmd)
+
+    if ex_code == 0:
+        click.secho("Success! Palm has been updated.", fg="green")
     else:
-        upgrade_cmd = f"pip install palm"
-
-    for cmd in (
-        uninstall_cmd,
-        upgrade_cmd,
-    ):
-        subprocess.run(shlex.split(cmd), check=True)
-
-    click.secho("Success! Palm has been updated.", fg="green")
+        click.secho("Failed to update palm", fg="red")
+        sys.exit(ex_code)
